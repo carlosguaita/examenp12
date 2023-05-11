b7ff3c1a537e5a548d10309707de77ec32fe6672
carlosguaita <120821872+carlosguaita@users.noreply.github.com>
2023-05-11 15:41:08 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,4 +1,1 @@
-float ingresarCoordenadaX();
-float ingresarCoordenadaY();
-float calcularDistancia(float x1, float y1, float x2, float y2);
-float areaTrianguloInterno(float a, float b, float c);
+
